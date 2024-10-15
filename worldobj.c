ceba396b97e29f9cb1371b27416e7a746bb21201
atomic-cogwheel0 <atomic.cogwheel0@gmail.com>
2024-10-15 22:04:41 +0200

    can't even remove a feature correctly

--- worldobj.c
+++ worldobj.c
@@ -24,15 +24,26 @@ int add_tank(world_obj *tank, llist l) {
 	return S_SUCCESS;
 }
 
+#pragma inline(acos_f)
+fixed acos_f(fixed f) {;
+	return float2f(acos(f2float(f)));
+}
+
 int tick_tank(world_obj *tank, llist l, world_obj *player, fixed timescale) {
 	camera *pl_cam;
 	vec3f cam_pos_noy, tank_pos_noy;
-	vec3f cam_to_tank, scr_normal;
+	vec3f cam_to_tank, z_axis;
+	fixed prod;
 	pl_cam = (*(camera **)player->data);
 
 	cam_pos_noy = remove_y_data(pl_cam->pos);
 	tank_pos_noy = remove_y_data(subvv(tank->mesh->pos, tank->mesh->ctr));
 
+	cam_to_tank = normalize(subvv(tank_pos_noy, cam_pos_noy));
+	z_axis = ivec3f(0, 0, int2f(1));
+
+	tank->mesh->yaw = float2f(90*DEG2RAD_MULT) + acos_f(dotp(cam_to_tank, z_axis));
+	
 	// move tank towards player (does not rotate yet)
 	tank->mesh->pos = addvv(tank->mesh->pos, mulvf(normalize(subvv(cam_pos_noy, tank_pos_noy)), divfi(timescale, 8)));
 }
